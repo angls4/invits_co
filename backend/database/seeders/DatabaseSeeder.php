468401ba3056978e2af249fe606488a212c8be61
WafiFz <wafi20002@mail.unpad.ac.id>
2023-10-14 11:12:53 +0700

    feat: model User & Userprofile -ms

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call('Database\Seeders\UserSeeder');
         $this->call('Database\Seeders\PackageSeeder');
     }
 }
